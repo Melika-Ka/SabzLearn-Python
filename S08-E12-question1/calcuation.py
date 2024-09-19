476e50cfab28c867db06cebdb6a657c1fda09c8f
Melika-Ka <melika78karimi@gmail.com>
2024-09-19 23:29:24 +0330

    add new practice of python module

---
+++ S08-E12-question1/calcuation.py
@@ -0,0 +1,12 @@
+# import mathOperations
+# mathOperations.suming(2, 3)
+# mathOperations.minus(20, 3)
+# mathOperations.divide(4, 2)
+# mathOperations.multiple(2, 4)
+
+from mathOperations import *
+
+suming(10, 2)
+minus(10, 2)
+divide(10, 2)
+multiple(10, 2)
---
+++ S08-E12-question1/mathOperations.py
@@ -0,0 +1,14 @@
+def suming(a, b):
+    print(a + b)
+
+
+def minus(a, b):
+    print(a - b)
+
+
+def multiple(a, b):
+    print(a * b)
+
+
+def divide(a, b):
+    print(a / b)
